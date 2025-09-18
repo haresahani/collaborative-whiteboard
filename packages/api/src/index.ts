191d075cafaeebf8e601559c4e08dd01fa3fcfb3
Hare Sahani <harecareer@gmail.com>
2025-09-18 23:27:41 +0530

    basic setup of api

---
+++ packages/api/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+const PORT = process.env.dev || 4000;
+
+app.get("/", (req, res) => {
+    res.send("Hellow from the API!");
+})
+
+app.listen(PORT, () => {
+    console.log(`API is running at http://localhost:${PORT}`);
+})
