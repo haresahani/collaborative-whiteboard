e4d719866a12d71757f7dce0bc02167fc4bc16bd
Hare Sahani <harecareer@gmail.com>
2026-03-07 00:49:25 +0530

    feat(api): setup express server, mongodb atlas connection, dotenv configuration

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,12 +1,23 @@
 import express from "express";
-const app = express();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
 
-const PORT = process.env.dev || 4000;
-
-app.get("/", (req, res) => {
-  res.send("Hellow from the API!");
+dotenv.config({
+  path: path.resolve(process.cwd(), "../../env/dev.env"),
 });
 
-app.listen(PORT, () => {
-  console.log(`API is running at http://localhost:${PORT}`);
+const app = express();
+
+app.use(express.json()); //json to js req.body
+app.use(express.urlencoded({ extended: true })); //url
+
+const PORT = process.env.PORT || 1111;
+
+await mongoose.connect(process.env.MONGO_URL!).then(() => {
+  // console.log(process.env.MONGO_URL);
+  console.log("MongoDB Connected!");
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
